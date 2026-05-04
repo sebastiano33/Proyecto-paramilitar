d43f2f38b57ac191341d4f72a6357b4beb804185
Sebas <sebastianovaron55@gmail.com>
2026-05-03 22:36:05 -0500

    Total backend audit: stripped to bare minimum, deleted src, fixed package entry

--- src/config/env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 3000,
-    ADMIN_API_KEY: process.env.ADMIN_API_KEY || "clave_super_secreta",
-    NODE_ENV: process.env.NODE_ENV || 'development'
-};
--- src/routes/anuncios.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/anuncios.controller');
-
-router.get('/', controller.getAnuncios);
-router.post('/', controller.publicarAnuncio);
-
-module.exports = router;
